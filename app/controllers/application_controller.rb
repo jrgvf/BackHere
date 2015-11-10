a99be32d9544eeb4236c54af893c2e6242960fcc
Jorge Rodrigues <jrgvf@cin.ufpe.br>
2015-11-10 18:19:33 -0300

    Install CanCanCan to Project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+  include CanCan::ControllerAdditions
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # For rescue exceptions of CanCan AccessDenied
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { render nothing: true, status: :forbidden }
+      format.html { redirect_to main_app.root_url, :alert => exception.message }
+    end
+  end
+
 end
