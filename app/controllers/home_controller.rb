d03964c2082bfc07ac011230717eb9c2b078a792
Jorge Rodrigues <jrgvf@cin.ufpe.br>
2015-12-05 16:01:41 -0300

    Resolve problem with Heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,12 +12,12 @@ class HomeController < ApplicationController
     @home_message = HomeMessage.new(home_message_params)
     respond_to do |format|
       if @home_message.save
-        # begin
+        begin
           HomeMessageMailer.home_message(@home_message).deliver_now
-        # rescue Exception => e
-        #   @home_message.destroy
-        #   format.json { render json: "Aconteceu algum problema e a mensagem não foi enviada. <br /><br /> Tente novamente em alguns instantes.", status: :internal_server_error}
-        # end
+        rescue Exception => e
+          @home_message.destroy
+          format.json { render json: "Aconteceu algum problema e a mensagem não foi enviada. <br /><br /> Tente novamente em alguns instantes.", status: :internal_server_error}
+        end
         format.json { render json: @home_message }
       else
         format.json { render json: errors_response(@home_message.errors.full_messages), status: :unprocessable_entity }
@@ -27,20 +27,20 @@ class HomeController < ApplicationController
 
   private
 
-  def errors_response errors_messages
-    response = String.new
-    errors_messages.each { |msg| response += msg + " <br />"}
-    response
-  end
+    def errors_response errors_messages
+      response = String.new
+      errors_messages.each { |msg| response += msg + " <br />"}
+      response
+    end
 
-  def sanitize_params
-    params.require(:home_message).each { |param, value| value.strip! }
-    params.require(:home_message)[:fone].gsub!(/[\W,_]/,'')
-    params.require(:home_message)[:name].downcase!
-    params.require(:home_message)[:email].downcase!
-  end
+    def sanitize_params
+      params.require(:home_message).each { |param, value| value.strip! }
+      params.require(:home_message)[:fone].gsub!(/[\W,_]/,'')
+      params.require(:home_message)[:name].downcase!
+      params.require(:home_message)[:email].downcase!
+    end
 
-  def home_message_params
-    params.require(:home_message).permit(:name, :email, :fone, :message)
-  end
+    def home_message_params
+      params.require(:home_message).permit(:name, :email, :fone, :message)
+    end
 end
