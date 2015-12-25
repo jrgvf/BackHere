0bba1dc93f67d248c82f2cb1ec1cd8f4cfb5b525
Jorge Rodrigues <jrgvf@cin.ufpe.br>
2015-12-25 16:52:30 -0300

    Other changes

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -44,9 +44,18 @@ class Seller
   attr_accessor :delete_avatar
   before_validation { self.avatar.clear if self.delete_avatar == '1' }
 
-  validates_attachment :avatar, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  validates_attachment_content_type :avatar, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-  validates_presence_of :name, :position#, :avatar
+  validates :name, presence: true
 
+  validates :position, presence: true, length: { maximum: 30 }
+
+  after_validation :clean_paperclip_errors
+
+  private
+
+    def clean_paperclip_errors
+      errors.delete(:avatar)
+    end
 
 end
