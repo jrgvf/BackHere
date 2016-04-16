870f46cb246d2ed9da7625f58f54929b5a49b08d
Jorge Rodrigues <jrgvf@cin.ufpe.br>
2016-04-16 17:19:26 -0300

    Fix refresh statuses button

--- app/assets/javascripts/backhere/status.js
+++ app/assets/javascripts/backhere/status.js
@@ -8,13 +8,24 @@ $(function() {
   });
 
   $('#update_all_status').on('click', function() {
+    var should_animate = true;
     var button_text = $('#update_all_status span').attr('button-text');
-    $('i.fa-refresh').addClass("fa-spin fa-lg");
-    $('#update_all_status span').text('');
-    window.setTimeout(function(){
-      $('i.fa-refresh').removeClass("fa-spin fa-lg");
-      $('#update_all_status span').text(button_text);
-    }, 1000);
+
+    $(document).on('ajaxStart', function() {
+      if (should_animate) {
+        $('#update_all_status span').text('');
+        $('i.fa-refresh').addClass("fa-spin fa-lg");        
+      };
+    });
+
+    $(document).on('ajaxStop',  function() {
+      if (should_animate) {
+        $('i.fa-refresh').removeClass("fa-spin fa-lg");
+        $('#update_all_status span').text(button_text);
+        should_animate = false;
+      };
+    });
+
     $('form.edit_status').each(function() {
       $(this).submit();
     });
