e6efd276ef7a3a4842e8545b870f4b185b64aea7
Jorge Rodrigues <jrgvf@cin.ufpe.br>
2016-07-05 01:45:05 -0300

    Fix redis for Sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5, driver: :hiredis }
+  config.redis = { url: ENV['REDIS_URL'], password: ENV['REDIS_PASSWORD'], network_timeout: 5, driver: :hiredis }
   # config.error_handlers << Proc.new {|ex,ctx_hash| MyErrorService.notify(ex, ctx_hash) }
 
   schedule_file = "config/schedule.yml"
@@ -9,7 +9,7 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5, driver: :hiredis }
+  config.redis = { url: ENV['REDIS_URL'], password: ENV['REDIS_PASSWORD'], network_timeout: 5, driver: :hiredis }
 end
 
 Sidekiq.default_worker_options = { 'backtrace' => true }
